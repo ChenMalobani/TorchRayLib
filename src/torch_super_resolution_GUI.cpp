eccc15abe2fbb63610556f78c4edce441bc96c49
Shlomo Kashani <shlomo@bayesian.io>
2020-10-01 07:07:24 +0300

    Load 4 models

--- src/torch_super_resolution_GUI.cpp
+++ src/torch_super_resolution_GUI.cpp
@@ -49,28 +49,42 @@ int main()
         std::cout << "Running on a CPU" << std::endl;
     }
     torch::Device device(device_type);
-    const std::string modelNameUdnie = "RRDB_ESRGAN_x4_000.pt";
+    const std::string modelNameESRGAN = "RRDB_ESRGAN_x4_000.pt";
+    const std::string modelNameCandy = "candy_cpp.pt";
+    const std::string modelNameMosaic = "mosaic_cpp.pt";
+    const std::string modelNameUdnie = "udnie_cpp.pt";
+
+    auto moduleESRGAN = torch::jit::load(modelNameESRGAN, device);
+    auto moduleCandy = torch::jit::load(modelNameCandy, device);
+    auto moduleMosaic = torch::jit::load(modelNameMosaic, device);
     auto moduleUdnie = torch::jit::load(modelNameUdnie, device);
+
     torch::NoGradGuard no_grad_guard;
     at::init_num_threads();
 
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1200;
-    const int screenHeight = 800;
+    const int screenHeight = 900;
 
     const char * mainTitle="TorchRayLib: Super-Resolution";
     InitWindow(screenWidth, screenHeight, mainTitle);
     gamefont = LoadFont("GameCube.ttf");
+    Sound clickSound = LoadSound("click.ogg");
+    Sound saveImageSound = LoadSound("save.ogg");
+
     GuiSetFont(gamefont);
-    GuiSetStyle(DEFAULT, TEXT_SIZE, 16);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 26);
     GuiFade(0.9f);
     GuiSetStyle(DEFAULT, TEXT_SPACING, 3);
+    Color defTextCLR= GetColor(GuiGetStyle(DEFAULT, LINE_COLOR));
 
-    // Configure the GUI styles.
+    // Background
+    GuiPanel(Rectangle{0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()});
+//    GuiLabel((Rectangle){padding + buttonWidth, padding, GetScreenWidth() - buttonWidth, buttonHeight}, " ...or Drag and Drop.");
+
+
 //    GuiLoadStyleDefault();
-
-    // Cyber Style
     GuiSetStyle(0, 0, 0xf0f0f0ff);
     GuiSetStyle(0, 1, 0x868686ff);
     GuiSetStyle(0, 2, 0xe6e6e6ff);
@@ -87,38 +101,34 @@ int main()
     GuiSetStyle(0, 17, 0x00000001);
     GuiSetStyle(0, 18, 0x9dadb1ff);
     GuiSetStyle(0, 19, 0x6b6b6bff);
-
-
 //    GuiLoadStyleDefault();
 //    GuiSetFont(GetFontDefault());
 //    GuiSetStyle(TEXTBOX, TEXT_ALIGNMENT, GUI_TEXT_ALIGN_CENTER);
-
     // GUI controls initialization
     //----------------------------------------------------------------------------------
     Rectangle windowBoxRec = { screenWidth/2 - 110, screenHeight/2 - 100, 220, 190 };
     bool windowBoxActive = false;
-
     int fileFormatActive = 0;
     const char *fileFormatTextList[1] = { ".png"};
-
     int pixelFormatActive = 0;
     const char *pixelFormatTextList[1] = {"RGB"};
-
     bool textBoxEditMode = false;
     char fileName[64] = "ESRGAN";
     //--------------------------------------------------------------------------------------
-
     Image image = { 0 };
     Texture2D texture = { 0 };
-
     bool imageLoaded = false;
     float imageScale = 1.0f;
     Rectangle imageRec = { 0.0f };
-
     bool btnExport = false;
-
     SetTargetFPS(60);
+    InitAudioDevice();
     //--------------------------------------------------------------------------------------
+
+//    while (!WindowShouldClose()) {
+//        if (scene == 1) Splashscreen();
+//        if (scene == 2) exit(0);
+//    }
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -134,19 +144,14 @@ int main()
             if (fileCount == 1)
             {
                 Image imTemp = LoadImage(droppedFiles[0]);
-
                 if (imTemp.data != NULL)
                 {
                     UnloadImage(image);
                     image = imTemp;
-
                     UnloadTexture(texture);
                     texture = LoadTextureFromImage(image);
-
                     imageLoaded = true;
-//                    pixelFormatActive = image.format - 1;
                     pixelFormatActive = UNCOMPRESSED_R8G8B8A8;
-
                     if (texture.height > texture.width) imageScale = (float)(screenHeight - 100)/(float)texture.height;
                     else imageScale = (float)(screenWidth - 100)/(float)texture.width;
                 }
@@ -159,26 +164,19 @@ int main()
         {
             if (imageLoaded)
             {
-//                ImageFormat(&image, pixelFormatActive + 1);
 //                ImageFormat(&image,UNCOMPRESSED_R8G8B8A8);
                 if ((GetExtension(fileName) == NULL) || (!IsFileExtension(fileName, ".png"))) strcat(fileName, ".png\0");     // No extension provided
-//                image = LoadImage(fileName);   // Loaded in CPU memory (RAM)
-                VU.applyModelOnImage(device, moduleUdnie,  image);
                 ExportImage(image,fileName);
-//                UnloadImage(image);
                 UnloadTexture(texture);
                 texture = LoadTextureFromImage(image);
-//                ExportImage(image, fileName);
-//                    UnloadImage(image);
-//                    image=imageGAN;
             }
             windowBoxActive = false;
         }
         if (imageLoaded)
         {
-            imageScale += (float)GetMouseWheelMove()*0.05f;   // Image scale control
+            imageScale += (float)GetMouseWheelMove()*0.20f;   // Image scale control
             if (imageScale <= 0.1f) imageScale = 0.1f;
-            else if (imageScale >= 5) imageScale = 5;
+            else if (imageScale >= 3) imageScale = 3;
 
             imageRec = Rectangle{ screenWidth/2 - (float)image.width*imageScale/2,
                                   screenHeight/2 - (float)image.height*imageScale/2,
@@ -189,25 +187,65 @@ int main()
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-//            ClearBackground(RAYWHITE);
         ClearBackground(GRAY);
         DrawText(mainTitle, 20, 20, 20, BLACK);
 
         if (texture.id > 0)
         {
-            DrawTextureEx(texture, Vector2{ screenWidth/2 - (float)texture.width*imageScale/2, screenHeight/2 - (float)texture.height*imageScale/2 }, 0.0f, imageScale, WHITE);
+            DrawTextureEx(texture, Vector2{ screenWidth/2 - (float)texture.width*imageScale/2,
+                                            screenHeight/2 - (float)texture.height*imageScale/2 }, 0.0f, imageScale, WHITE);
 
             DrawRectangleLinesEx(imageRec, 1, CheckCollisionPointRec(GetMousePosition(), imageRec) ? RED : DARKGRAY);
-            DrawText(FormatText("SCALE: %.2f%%", imageScale*100.0f), 20, screenHeight - 40, 20, GetColor(GuiGetStyle(DEFAULT, LINE_COLOR)));
+            DrawText(FormatText("SCALE: %.2f%%", imageScale*100.0f), 20, screenHeight - 40, 20,defTextCLR);
         }
+
         else
         {
-            DrawText("TO RUN A PYTORCH SUPER RESOLUTION MODEL, DRAG & DROP YOUR IMAGE", 30, 200, 20, BLACK);
+            DrawText("PYTORCH SUPER RESOLUTION, DRAG & DROP AN IMAGE.", 20, 150, 20,defTextCLR);
             GuiDisable();
         }
 
-        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight - 50, 150, 30 }, "SR Image")) windowBoxActive = true;
+        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight - 250, 150, 30 }, "SAVE")) {
+            PlaySound(saveImageSound);
+            TraceLog(LOG_INFO, "TorchRaLib: save image");
+            windowBoxActive = true;
+        }
+
+
+        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight - 50, 150, 30 }, "SR")) {
+            PlaySound(clickSound);
+            TraceLog(LOG_INFO, "TorchRaLib: ESRGAN");
+            VU.applyModelOnImage(device, moduleESRGAN, image);
+//            ExportImage(image,fileName);
+            UnloadTexture(texture);
+            texture = LoadTextureFromImage(image);
+            PlaySound(saveImageSound);
+//            windowBoxActive = true;
+        }
+
+        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight - 90, 150, 30 }, "Candy")) {
+            PlaySound(clickSound);
+            TraceLog(LOG_INFO, "TorchRaLib: Candy");
+            VU.applyModelOnImage(device, moduleCandy, image);
+            UnloadTexture(texture);
+            texture = LoadTextureFromImage(image);
+        }
+
+        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight - 130, 150, 30 }, "Mosaic")) {
+            PlaySound(clickSound);
+            TraceLog(LOG_INFO, "TorchRaLib: Mosaic");
+            VU.applyModelOnImage(device, moduleMosaic, image);
+            UnloadTexture(texture);
+            texture = LoadTextureFromImage(image);
+        }
+
+        if (GuiButton(Rectangle{ screenWidth - 170, screenHeight -170, 150, 30 }, "Darken")) {
+            PlaySound(clickSound);
+            TraceLog(LOG_INFO, "TorchRaLib: Darken");
+            ImageColorBrightness(&image, -40);
+            texture = LoadTextureFromImage(image);
+        }
+
         GuiEnable();
 
         // Draw window box: windowBoxName
